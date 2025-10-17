4a2a3ada9b73fa3433d8bf437c4086ef2a4b7f3c
bunnmei <kombinat.web@gmail.com>
2025-10-18 00:57:07 +0900

    FolderMake

--- src/app/dialog/dialog.ts
+++ src/app/dialog/dialog.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { DataShare } from '../data-share';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.html',
-  styleUrls: ['./dialog.css']
+  styleUrls: ['./dialog.css'],
+  imports: [FormsModule],
 })
 export class Dialog {
   constructor(
-    public dataShare: DataShare
+    public dataShare: DataShare,
+    private http: HttpClient,
   ) { }
 
+  newFolderName = ''
 
   closeDialog() {
     this.dataShare.dialogType.set('none')
@@ -18,5 +22,25 @@ export class Dialog {
 
   none() {
     console.log("clicked dialog panel")
+  }
+
+  createFolder() {
+    const apiUrl = 'http://192.168.1.112:8080/create_folder';
+    if(this.newFolderName === "") return
+    this.http
+      .post(apiUrl, {
+        path: this.dataShare.currentPath(),
+        folderName: this.newFolderName
+      })
+      .subscribe({
+        // 成功時の処理（例: サーバーが新しく作成されたフォルダを返した場合）
+        next: (response) => {
+          console.log('フォルダ作成成功:', response);
+        },
+        // 失敗時の処理
+        error: (err) => {
+          console.error('フォルダ作成エラー:', err);
+        }
+      });
   }
 }
